d1aabd0c949cc42a600d2ad96788c1c0c4982b87
kosbog <johnkot@alterpost.org>
2017-08-30 18:32:38 +0300

    WIP

--- app/components/About.js
+++ app/components/About.js
@@ -3,7 +3,35 @@ import React from 'react';
 const About = () => {
     return (
         <section className="about">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum illum itaque pariatur repellat eaque animi, qui minima amet. Quo repellat deleniti, molestias. Laudantium deleniti ex architecto? Accusantium eius, consectetur velit.
+            <div className="about-wrapper">
+                <div className="about-info">
+                    <div className="about-headers">
+                        <h2 className="about-headers-title">Art of cakes</h2>
+                        <h3 className="about-headers-subtitle">we create delicious memories</h3>
+                    </div>
+                    <div className="about-text">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                        Deserunt accusantium earum asperiores ullam consequatur quasi at
+                        facilis, dolores, impedit, esse dolore ut
+                        molestias sint suscipit accusamus? Reprehenderit soluta nostrum veniam!
+                    </div>
+                    <div className="about-chef">
+                        <h4 className="about-chef-title">Chef Cook</h4>
+                        <div className="about-chef-name">
+                            <span>Firstname</span>
+                            <img src="" alt="" />
+                            <span>Lastname</span>
+                        </div>
+                        <div className="about-chef-quote"></div>
+                    </div>
+                </div>
+                <div className="about-image">
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                </div>
+            </div>
         </section>
     )
 }
