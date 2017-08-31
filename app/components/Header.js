ad26149fd1fc7470b6f14073e2b403675056acaf
kosbog <johnkot@alterpost.org>
2017-08-31 14:26:22 +0300

    WIP

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
+const BakeyLabel = () => {
+    return (
+        <div className="bakery-label"></div>
+    )
+}
+
 const Navigation = () => {
     return (
         <nav>
@@ -41,6 +47,7 @@ const Header = () => {
             <h1 className="header-title">
                 Good Bakery Shop
             </h1>
+            <BakeyLabel />
         </header>
     )
 }
