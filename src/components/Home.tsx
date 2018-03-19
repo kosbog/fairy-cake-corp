4c0a98bf3e67d22587b2ac65627d8dcc219efe45
kosbog <badykos@gmail.com>
2018-03-19 08:42:27 +0200

    wip: Butoon, Divider components

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,6 +2,12 @@ import * as React from "react";
 import Alert from "./common/Alert";
 import Divider from "./common/Divider";
 import Button from "./common/Button";
+const divider_1 = require('../assets/images/divider_1.png');
+
+const divider_2 = require('../assets/images/divider_2.png');
+
+const divider_3 = require('../assets/images/divider_3.png');
+
 
 class Home extends React.Component<{}, {}> {
     constructor(props: {}) {
@@ -18,7 +24,9 @@ class Home extends React.Component<{}, {}> {
                 <Alert type={"error"} icon={"ban"} content={"Error box"} />
                 <Alert type={"info"} icon={"info"} content={"Information box"} />
                 <Alert type={"confirm"} icon={"check"} content={"Confirmation box"} />
-                <Divider type={"divider_1"} />
+                <Divider type={divider_1} />
+                <Divider type={divider_2} />
+                <Divider type={divider_3} />
                 <Button type={"large"}/>
                 <Button type={"medium"}/>
                 <Button type={"small"}/>
--- src/components/common/Divider.tsx
+++ src/components/common/Divider.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+const divider_1 = require('../../assets/images/divider_1.png');
 
 interface IProps {
     type: string;
@@ -7,7 +8,7 @@ interface IProps {
 const Divider = ({ type }: IProps) => {
     return (
         <div className="divider">
-            <img src={type} alt={type} />
+            <img src={type} alt="divider" className="crossline" />
         </div>
     );
 };
