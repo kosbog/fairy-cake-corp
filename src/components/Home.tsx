c0a57af2f7944bc487c5b87c82b80111613e953c
Bogdan Kositskiy <kosbog@MacBook-Pro-Bogdan.local>
2018-03-19 17:43:00 +0200

    chore: update tslint.json

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,12 +2,11 @@ import * as React from "react";
 import Alert from "./common/Alert";
 import Divider from "./common/Divider";
 import Button from "./common/Button";
-const divider_1 = require('../assets/images/divider_1.png');
+const divider1 = require("../assets/images/divider_1.png");
 
-const divider_2 = require('../assets/images/divider_2.png');
+const divider2 = require("../assets/images/divider_2.png");
 
-const divider_3 = require('../assets/images/divider_3.png');
-
+const divider3 = require("../assets/images/divider_3.png");
 
 class Home extends React.Component<{}, {}> {
     constructor(props: {}) {
@@ -24,9 +23,9 @@ class Home extends React.Component<{}, {}> {
                 <Alert type={"error"} icon={"ban"} content={"Error box"} />
                 <Alert type={"info"} icon={"info"} content={"Information box"} />
                 <Alert type={"confirm"} icon={"check"} content={"Confirmation box"} />
-                <Divider type={divider_1} />
-                <Divider type={divider_2} />
-                <Divider type={divider_3} />
+                <Divider type={divider1} />
+                <Divider type={divider2} />
+                <Divider type={divider3} />
                 <Button type={"large"}/>
                 <Button type={"medium"}/>
                 <Button type={"small"}/>
